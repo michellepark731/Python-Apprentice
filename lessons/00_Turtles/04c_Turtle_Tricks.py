d091c990c6290e6ab2f0eb4168015f3ee3b5b504
michellepark731 <michellepark0731@gmail.com>
2025-01-21 01:35:38 +0000

    testing commit

--- lessons/00_Turtles/04c_Turtle_Tricks.py
+++ lessons/00_Turtles/04c_Turtle_Tricks.py
@@ -17,8 +17,8 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 # Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
 # Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
-
-
+name = "michelle"
+print(name)
 ... # Your code here
 
 turtle.exitonclick()                    # Close the window when we click on it
